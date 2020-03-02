2e4310d8802835f6b972d715381e4ae1bcf7a8b8
Cherfez <chervasev.leeuwen@gmail.com>
2020-03-02 13:27:04 +0100

    Added table with availability for doctors. Pnt 3 done

--- src/components/DutyPage.js
+++ src/components/DutyPage.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import OpeningTimes from "./OpeningTimes";
 
 export default function DutyPage() {
+  const [doctors, setDoctors] = useState([]);
+  const [load, setLoad] = useState("");
+
+  async function getDoctors() {
+    try {
+      setLoad("Loading");
+      const response = await axios.get(
+        "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/doctors"
+      );
+      console.log(response.data);
+
+      setLoad("");
+      setDoctors(response.data);
+    } catch (error) {
+      console.log("error message", error.message);
+    }
+  }
+
+  useEffect(() => {
+    getDoctors();
+  }, []);
+
   return (
     <div>
+      <p>{load}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Doctor</th>
+            <th>Availability</th>
+          </tr>
+        </thead>
+        <tbody>
+          {doctors.map(doctor => {
+            if (doctor.onDuty === true) {
+              return (
+                <tr key={doctor.id}>
+                  <td>{doctor.doctor}</td>
+                  <td>Available</td>
+                </tr>
+              );
+            } else {
+              return (
+                <tr key={doctor.id}>
+                  <td>{doctor.doctor}</td>
+                  <td>Not Available</td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </table>
       <div className="gp-info">
         <p>
           We are: <OpeningTimes />
         </p>
-        <p>To maken an appointment, please call: 020-555 5555</p>
+        <p>To make an appointment, please call: 020-555 5555</p>
       </div>
     </div>
   );
