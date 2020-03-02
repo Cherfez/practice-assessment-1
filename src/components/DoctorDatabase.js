3f7f868bab0fbdf5a684d2eee20dc4e154d41955
Cherfez <chervasev.leeuwen@gmail.com>
2020-03-02 16:06:09 +0100

    beginning of doctor database. filter function not working

--- src/components/DoctorDatabase.js
+++ src/components/DoctorDatabase.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PatientInfo from "../PatientInfo";
 
 export default function DoctorDatabase() {
-  return <div>DATABASE FOR DOCTORS</div>;
+  const [doctor, setDoctor] = useState([]);
+  const [sort_by, set_sort_by] = useState("");
+
+  //checks for dr id value
+  function doctorWasChanged(e) {
+    console.log("event", e.target.value);
+    set_sort_by(e.target.value);
+  }
+
+  async function getDoctor() {
+    try {
+      const response = await axios.get(
+        "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/doctors"
+      );
+      console.log("doctors", response.data);
+
+      setDoctor(response.data);
+    } catch (error) {
+      console.log("error message", error.message);
+    }
+  }
+  useEffect(() => {
+    getDoctor();
+  }, []);
+
+  return (
+    <div>
+      <h1>Patient Database</h1>
+      <label>Doctor</label>
+      <select onChange={doctorWasChanged}>
+        {doctor.map(doc => {
+          return (
+            <option value={doc.id} key={doc.id}>
+              {doc.doctor}
+            </option>
+          );
+        })}
+      </select>
+      <PatientInfo />;
+    </div>
+  );
 }
