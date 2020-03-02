f817c8459226a36dcc18532c95f2a0f90d9f77f3
Cherfez <chervasev.leeuwen@gmail.com>
2020-03-02 17:45:16 +0100

    homepage component ready

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import OpeningTimes from "./OpeningTimes";
 
 export default function HomePage() {
-  // const [openclose, setOpenclose] = useState("");
-
-  // const checkTime = () => {
-  //   let now = new Date();
-
-  //   let dayOfWeek = now.getDay(); // 0 = Sun, 1 = Mon, ... 6 = Sat
-  //   let hour = now.getHours(); // 0 = 12am, 1 = 1am, ... 18 = 6pm
-
-  //   // check if it's a weekday between 9am and 6pm
-  //   if (dayOfWeek > 0 && dayOfWeek < 6 && hour >= 8 && hour < 12) {
-  //     setOpenclose("Open!");
-  //   } else {
-  //     setOpenclose("Closed!");
-  //   }
-  // };
-
-  // setInterval(checkTime, 1000);
-  // console.log(setInterval(checkTime, 1000));
-  // //checkTime();
-
   return (
     <div>
       <h1>Welcome to AMS GPs</h1>
       <div className="gp-info">
         <p>
-          We are: {/*<span className="open-close">{openclose}</span>*/}{" "}
-          <OpeningTimes />
+          We are: <OpeningTimes />
         </p>
         <p>To maken an appointment, please call: 020-555 5555</p>
       </div>
