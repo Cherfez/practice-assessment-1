af15d6c0d2be9cb720bdb225d9fdc980ea96b664
Cherfez <chervasev.leeuwen@gmail.com>
2020-03-03 12:02:24 +0100

    finishing Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
       <ul>
         <li>
-          <NavLink exact={true} activeStyle={{ color: "yellow" }} to="/">
+          <NavLink
+            exact={true}
+            activeStyle={{ fontWeight: "bold" }}
+            to="/"
+            className="navlink"
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink exact={true} activeStyle={{ color: "green" }} to="/duty">
+          <NavLink
+            exact={true}
+            activeStyle={{ fontWeight: "bold" }}
+            to="/duty"
+            className="navlink"
+          >
             Doctors Schedule
           </NavLink>
         </li>
         <li>
-          <NavLink exact={true} activeStyle={{ color: "red" }} to="/signup">
+          <NavLink
+            exact={true}
+            activeStyle={{ fontWeight: "bold" }}
+            to="/signup"
+            className="navlink"
+          >
             New Patients
           </NavLink>
         </li>
         <li>
-          <NavLink exact={true} activeStyle={{ color: "purple" }} to="/doctor">
+          <NavLink
+            exact={true}
+            activeStyle={{ fontWeight: "bold" }}
+            to="/doctor"
+            className="navlink"
+          >
             Database per Doctor
           </NavLink>
         </li>
